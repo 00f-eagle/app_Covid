ff18972cbe2e682a1437a8d70889d08c40916316
Kirill Selivanov <sks.kirill@gmail.com>
2020-06-30 11:08:53 +0300

    initial commit

---
+++ Covid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid
+//
+//  Created by Kirill Selivanov on 30.06.2020.
+//  Copyright © 2020 Kirill Selivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
